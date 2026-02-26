497467a587113bcd26ee46a3c1a1843ecabd09b6
Cristina <anajeram@miumg.edu.gt>
2026-02-25 23:34:45 -0600

    Segundo commit

--- Analizador lexico/obj/Debug/net8.0/Analizador lexico.AssemblyInfo.cs
+++ Analizador lexico/obj/Debug/net8.0/Analizador lexico.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Analizador lexico")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d41526bce0410c3cbd62f679d38c18a19dbc0622")]
 [assembly: System.Reflection.AssemblyProductAttribute("Analizador lexico")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Analizador lexico")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
